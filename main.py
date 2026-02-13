a83a834f34996baf1e5d087e5b0568f472207b1f
Tion <tion.jamesdominic@gmail.com>
2026-02-13 17:55:26 +0800

    added json loading for salary json

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import deduction_rates as rates
 import task_parallelism
-
+import json
 
 
 funcs = [rates.compute_sss, rates.compute_philhealth, rates.calculate_pagibig, rates.calculate_tax]
+
+salaries = []
+
+try:
+    with open("data/salary.json", "r") as file:
+        employees = json.load(file)
+        salaries = [emp["salary"] for emp in employees]
+except:
+    print("Error, maybe try running generate salary.py")
+
+
 
 if __name__ == "__main__":
     task_parallelism.task_p(funcs, 5000)
